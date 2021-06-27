d1a43ce4261177c3716b53df8495a28670d6ee4b
St.Onge <stonge.ms@gmail.com>
2021-06-26 23:28:14 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+git add -const fs = require("fs/promises");
+
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(__dirname + "/public/notes.html");
+});
+
+// GET request to /api/notes
+app.get("/api/notes", async function (req, res) {
+  try {
+    const data = await fs.readFile("./db/db.json", "utf8");
+    res.json(JSON.parse(data));
+  } catch (err) {
+    res.status(500).end("Server failed");
+  }
+});
+
+// POST request to /api/notes
+app.post("/api/notes", async function (req, res) {
+  const notes = req.body;
+  try {
+    const data = await fs.writeFile("./db/db.json", "utf8");
+    res.json(JSON.parse(data));
+    data.push(notes);
+  } catch (err) {
+    res.status(500).end("Server failed");
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
